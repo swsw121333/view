4accf255ef99cf96971c221478326ec8b3cfae61
swsw121333 <swsw1213@nate.com>
2020-05-29 10:36:25 +0900

    ㅁㅇㅇ

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
-  client.user.setPresence({ game: { name: '!help를 쳐보세요.' }, status: 'online' })
+  client.user.setPresence({ game: { name: '무언가를' }, status: 'online' })
 });
 
 client.on("guildMemberAdd", (member) => {
